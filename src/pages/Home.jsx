3bea309ad06597d5def95fb3400d1033a443065b
Altamash sheikh <99234921+SkAltmash@users.noreply.github.com>
2025-06-06 23:08:41 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,21 +7,29 @@ const Home = () => {
       <HeroSection />
       <div className="p-4 bg-gray-50 dark:bg-black transition-colors duration-300">
         <MediaSection
-          title="Now Playing Movies"
-       fetchUrl="/.netlify/functions/tmdb-proxy?endpoint=tv/on_the_air"
-        />
-        <MediaSection
-          title="Popular Web Shows"
-          fetchUrl="/.netlify/functions/tmdb-proxy?endpoint=tv/popular"
-        />
-        <MediaSection
-          title="Top Rated Movies"
-          fetchUrl="/.netlify/functions/tmdb-proxy?endpoint=movie/top_rated"
-        />
-        <MediaSection
-          title="Currently Airing Shows"
-      fetchUrl="/.netlify/functions/tmdb-proxy?endpoint=tv/on_the_air"
-        />
+  title="Now Playing Movies"
+  fetchUrl="/.netlify/functions/tmdb?path=movie/now_playing"
+/>
+
+<MediaSection
+  title="Popular Web Shows"
+  fetchUrl="/.netlify/functions/tmdb?path=tv/popular"
+/>
+
+<MediaSection
+  title="Top Rated Movies"
+  fetchUrl="/.netlify/functions/tmdb?path=movie/top_rated"
+/>
+
+<MediaSection
+  title="Currently Airing Shows"
+  fetchUrl="/.netlify/functions/tmdb?path=tv/on_the_air"
+/>
+
+<MediaSection
+  title="Trending Movies"
+  fetchUrl="/.netlify/functions/tmdb?path=discover/movie&query=sort_by=popularity.desc"
+/>
       </div>
     </>
   );

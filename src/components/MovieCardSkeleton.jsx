109ecae5f3811dc2a213201bf702a5399d531241
SkAltmash <skaltmash3@gmail.com>
2025-06-07 16:16:07 +0530

    passkey added

--- src/components/MovieCardSkeleton.jsx
+++ src/components/MovieCardSkeleton.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const MovieCardSkeleton = () => {
   return (
-    <div className="w-40 sm:w-48 h-80 bg-gray-200 dark:bg-gray-800 rounded-lg animate-pulse">
+    <div className="w-35 sm:w-48 h-80 bg-gray-200 dark:bg-gray-800 rounded-lg animate-pulse">
       <div className="h-56 bg-gray-300 dark:bg-gray-700 rounded-t-lg" />
       <div className="p-2 space-y-2">
         <div className="h-4 w-3/4 bg-gray-300 dark:bg-gray-600 rounded" />
